122826f40c16ac82aa4da0b6543effcc50a18e79
nelsonbulaun <108250880+nelsonbulaun@users.noreply.github.com>
2023-10-02 13:35:13 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import eslintPlugin from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
-  base: "/tetris/"
+  base: "/",
 })
